3cb8827fca4048bbca95e448f68b8b69da0d57b4
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-07-24 01:47:50 +0530

    Create the gridHandler function
    created a gridHandler function and moved the eventlistener and the generateGrid function inside that function

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function generateGrid(squareNo){
-    let squareLength = 960/squareNo;
-    let totalSquareNo = squareNo * squareNo;
-    let container = document.querySelector(".container");
-    for(let i = 1; i <= totalSquareNo; i++){
-        let square = document.createElement("div");
-        square.style.width = squareLength + "px";
-        square.style.height = squareLength + "px";
-        square.style.backgroundColor = "white";
-        container.appendChild(square);
-    }
-    return;    
-}
+function gridHandler(){
 
-const gridSizeBtn = document.querySelector("#grid_size_btn");
-let gridSize = gridSizeBtn.addEventListener("click",(e)=>{
-    let gridSize = prompt("Enter the grid size: ");
-    if(Number.isInteger(gridSize)){
-        return alert("Please enter a number to set the number of squares each side of the grid");
+    function generateGrid(squareNo){
+        let squareLength = 960/squareNo;
+        let totalSquareNo = squareNo * squareNo;
+        let container = document.querySelector(".container");
+        
+        for(let i = 1; i <= totalSquareNo; i++){
+            let square = document.createElement("div");
+            square.style.width = squareLength + "px";
+            square.style.height = squareLength + "px";
+            square.style.backgroundColor = "white";
+            square.setAttribute("class", "square");
+            container.appendChild(square);
+        }
+        return;    
     }
-    e.target.textContent = gridSize + "x" + gridSize;
-    return generateGrid(gridSize);
-})
+
+    const gridSizeBtn = document.querySelector("#grid_size_btn");
+    gridSizeBtn.addEventListener("click",(e)=>{
+        let gridSize = prompt("Enter the grid size: ");
+        if(Number.isInteger(gridSize)){
+            return alert("Please enter a number to set the number of squares each side of the grid");
+        }
+        e.target.textContent = gridSize + "x" + gridSize;
+        return generateGrid(gridSize);
+    })
+}
+gridHandler();
