265f91dbf492c4dd91fd4d5e5465a5dc206fea82
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-07-24 00:52:20 +0530

    Add the user input prompt for the grid size
    added event listener that prompts the user to set the grid size and calls the generateGrid function
    
    added the flex and flex-wrap properties to the container

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ function generateGrid(squareNo){
     let container = document.querySelector(".container");
     for(let i = 1; i <= totalSquareNo; i++){
         let square = document.createElement("div");
-        square.style.width = squareLength;
-        square.style.height = squareLength;
+        square.style.width = squareLength + "px";
+        square.style.height = squareLength + "px";
         square.style.backgroundColor = "white";
         container.appendChild(square);
     }
     return;    
 }
+
+const gridSizeBtn = document.querySelector("#grid_size_btn");
+let gridSize = gridSizeBtn.addEventListener("click",(e)=>{
+    let gridSize = prompt("Enter the grid size: ");
+    if(Number.isInteger(gridSize)){
+        return alert("Please enter a number to set the number of squares each side of the grid");
+    }
+    e.target.textContent = gridSize + "x" + gridSize;
+    return generateGrid(gridSize);
+})
