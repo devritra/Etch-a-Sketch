15c595dd1a885cdcee11f89d252c3491e2a4fa6a
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-07-24 16:21:43 +0530

    Add deafult grid
    created a function that adds a default grid of 16x16 from the start when the user hasn't set a size yet
    
    added some coditons to the eventlistener of the grid size button so that it can filter out empty strings as well along with non integers

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 function gridHandler(){
+    
+    function defaultGrid(){
+        let squareLength = 960/16;
+        let totalSquareNo = 16 * 16;
+        let container = document.querySelector(".container");
+        for(let i = 1; i <= totalSquareNo; i++){
+            let square = document.createElement("div");
+            square.style.width = squareLength + "px";
+            square.style.height = squareLength + "px";
+            square.style.backgroundColor = "white";
+            square.setAttribute("class", "square");
+            container.appendChild(square);
+        }
+        return;
+    }
 
     function generateGrid(squareNo){
         let squareLength = 960/squareNo;
         let totalSquareNo = squareNo * squareNo;
         let container = document.querySelector(".container");
-        
+        let previousSquares = document.querySelectorAll(".square");
+        previousSquares = Array.from(previousSquares);
+        previousSquares.map((item)=>item.remove());
         for(let i = 1; i <= totalSquareNo; i++){
             let square = document.createElement("div");
             square.style.width = squareLength + "px";
@@ -16,11 +33,14 @@ function gridHandler(){
         return;    
     }
 
+    defaultGrid();
     const gridSizeBtn = document.querySelector("#grid_size_btn");
     gridSizeBtn.addEventListener("click",(e)=>{
-        let gridSize = prompt("Enter the grid size: ");
-        if(Number.isInteger(gridSize)){
-            return alert("Please enter a number to set the number of squares each side of the grid");
+        let gridSize = Number(prompt("Enter the grid size: "));
+        if(!Number.isInteger(gridSize)){
+            return alert("Please enter a number to set the number of squares each side of the grid, for example, the default is: 16");
+        } else if(gridSize === 0) {
+            return alert("Please enter a number to set the number of squares each side of the grid, for example, the default is: 16");
         }
         e.target.textContent = gridSize + "x" + gridSize;
         return generateGrid(gridSize);
